c99246289fb22878028abfc29c981b6a8f5351aa
hasslodotcom <87302986+hasslodotcom@users.noreply.github.com>
2023-04-18 09:29:24 -0400

    Update java.js

--- js/java.js
+++ js/java.js
@@ -17,3 +17,39 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+$(function() {
+	
+	// Auto-selection when clicked
+	$(".search").click(function() {
+  		$(this).select();
+		$(".search").css('color', 'black');
+	});
+	
+	// Text turns gray again when not typing
+	$(".search").blur(function() {
+		$(".search").css('color', '#7A7A7A');
+	});
+	
+	// Expand search bar
+	$(".search").focus(function() {
+		$(this).animate({ width: "300px" }, 250);
+	});
+	
+	$(".search").blur(function() {
+		$(this).animate({ width: "150px" }, 200);
+	});
+	
+	// Search... dissapears when field is clicked
+	$('.search').focus(function() {
+	  if ($(this).val() === 'Search...') {
+	    return $(this).val('');
+	  }
+	});
+	
+	// Search... reappears when field is not in focus anymore
+    $('.search').blur(function() {
+      if ($(this).val() === '') {
+        return $(this).val('Search...');
+      }
+	});
